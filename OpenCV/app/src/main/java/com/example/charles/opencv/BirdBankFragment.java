2ac6d36cad77c41eef3e7bd4e848c79b0a00851b
josh <lowejosh50@gmail.com>
2018-05-11 14:01:43 +1100

    connected database (probably) however the listview still need sto be populated

--- OpenCV/app/src/main/java/com/example/charles/opencv/BirdBankFragment.java
+++ OpenCV/app/src/main/java/com/example/charles/opencv/BirdBankFragment.java
@@ -13,6 +13,10 @@ public class BirdBankFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+
+
+
         return inflater.inflate(R.layout.fragment_birdbank, null);
     }
 }
