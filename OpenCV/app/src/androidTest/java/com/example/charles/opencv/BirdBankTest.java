0565e97056a5c69741c6aa8a019f1a38c8bbb702
Your name <lowejosh50@gmail.com>
2018-06-04 17:03:41 +1000

    minor changes

--- OpenCV/app/src/androidTest/java/com/example/charles/opencv/BirdBankTest.java
+++ OpenCV/app/src/androidTest/java/com/example/charles/opencv/BirdBankTest.java
@@ -15,28 +15,9 @@ import org.junit.runner.RunWith;
  */
 @RunWith(AndroidJUnit4.class)
 public class BirdBankTest {
-    private Question question;
-    private String table = "Bill";
-    private String questionToAsk = "What Distinct Bill Shape does the Bird have?";
-
     @Before
     public void setup() {
-        question = new Question(table, questionToAsk);
-    }
 
-    @Test
-    public void getTableSuccess() {
-        Assert.assertEquals(table, question.getTable());
-    }
-
-    @Test
-    public void getFeatureSuccess() {
-        Assert.assertEquals(table, question.getFeature());
-    }
-
-    @Test
-    public void getQuestionSuccess() {
-        Assert.assertEquals(questionToAsk, question.getQuestion());
     }
 }
 
