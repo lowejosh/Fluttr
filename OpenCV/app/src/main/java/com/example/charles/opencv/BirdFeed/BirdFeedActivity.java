f9f32dbb5b7a769ff6055cac4112bec5de81851a
AshaDasha <30457605+AshaDasha@users.noreply.github.com>
2018-09-05 15:17:24 +1000

    Created Birdfeed xml and activity files WIP

---
+++ OpenCV/app/src/main/java/com/example/charles/opencv/BirdFeed/BirdFeedActivity.java
@@ -0,0 +1,16 @@
+package com.example.charles.opencv.BirdFeed;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class BirdFeedActivity extends AppCompatActivity  {
+
+
+}
