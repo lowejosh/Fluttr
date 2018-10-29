08e7be92b2dcc1797fc72359d67013d740063eba
Allcharles <charles@alleman.com.au>
2018-10-29 12:25:22 +1000

    Working Recorder

--- OpenCV/app/src/main/java/com/example/charles/opencv/FeatureActivity/RecordActivity.java
+++ OpenCV/app/src/main/java/com/example/charles/opencv/FeatureActivity/RecordActivity.java
@@ -1,16 +1,14 @@
 package com.example.charles.opencv.FeatureActivity;
 
 import android.Manifest;
-import android.annotation.TargetApi;
 import android.app.Activity;
-import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.media.MediaPlayer;
 import android.media.MediaRecorder;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -18,21 +16,20 @@ import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.charles.opencv.R;
 import com.example.charles.opencv.Recording.RecordingAdapter;
-import com.example.charles.opencv.Settings.ReferenceActivity;
-import com.example.charles.opencv.Settings.ResetActivity;
 
-import org.w3c.dom.Text;
+import static android.Manifest.permission.RECORD_AUDIO;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
 
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.List;
 
 /**
  * Shows the settings available to the user, this currently only displays the references for information
@@ -41,7 +38,7 @@ import java.util.List;
 public class RecordActivity extends AppCompatActivity {
     private static MediaRecorder mediaRecorder;
     private static MediaPlayer mediaPlayer;
-    private static SimpleDateFormat format = new SimpleDateFormat("dd-mm-yyyy hh:mm:ss");
+    private static SimpleDateFormat format = new SimpleDateFormat("ddmmyyyyhhmmss");
 
     private static String folder = "fluttr_audio";
 
@@ -49,56 +46,56 @@ public class RecordActivity extends AppCompatActivity {
     private Button stopButton;
     private TextView currentlyPlaying;
     private TextView currentlyPlayingAudio;
+    private TextView noRecordings;
 
-    private static final int REQUEST_EXTERNAL_STORAGE = 1;
-    private static String[] PERMISSIONS_STORAGE = {
-            Manifest.permission.READ_EXTERNAL_STORAGE,
-            Manifest.permission.WRITE_EXTERNAL_STORAGE
-    };
+    private static final int RequestPermissionCode = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.recording);
 
+        //Retrieve important objects
         recordButton = findViewById(R.id.startBtn);
         stopButton = findViewById(R.id.stopBtn);
         currentlyPlaying = findViewById(R.id.currently_playing);
         currentlyPlayingAudio = findViewById(R.id.currently_playing_audio);
+        noRecordings = findViewById(R.id.no_recordings);
 
-        if (!hasMicrophone()) {
-            stopButton.setEnabled(false);
-            stopButton.setEnabled(false);
-        } else {
-            stopButton.setEnabled(false);
-        }
-
-        verifyStoragePermissions(this);
+        stopButton.setEnabled(false);
+        currentlyPlaying.setVisibility(View.GONE);
+        currentlyPlayingAudio.setVisibility(View.GONE);
+        noRecordings.setVisibility(View.GONE);
 
         makeDirectory();
         updateAudioFiles();
     }
 
     public void recordAudio(View v) {
+        if (!checkPermission()) {
+            requestPermission();
+            return;
+        }
+
+        recordButton.setEnabled(false);
         stopButton.setEnabled(true);
 
         Date currentTime = Calendar.getInstance().getTime();
-
-        String audioFilePath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/" + folder + "/" + format.format(currentTime)+ " Recording.3pg";
+        String audioFilePath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/" + folder + "/" + format.format(currentTime)+ "Recording.mp3";
 
         try {
             mediaRecorder = new MediaRecorder();
             mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
             mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-            mediaRecorder.setOutputFile(audioFilePath);
             mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+            mediaRecorder.setOutputFile(audioFilePath);
             mediaRecorder.prepare();
+            mediaRecorder.start();
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-        mediaRecorder.start();
-        recordButton.setEnabled(false);
+        Toast.makeText(RecordActivity.this, "Recording Started", Toast.LENGTH_LONG).show();
     }
 
     public void stopAudio(View v) {
@@ -109,6 +106,7 @@ public class RecordActivity extends AppCompatActivity {
         recordButton.setEnabled(true);
 
         updateAudioFiles();
+        Toast.makeText(RecordActivity.this, "Recording Completed", Toast.LENGTH_LONG).show();
     }
 
     private void stopPlaying() {
@@ -117,12 +115,17 @@ public class RecordActivity extends AppCompatActivity {
         LinearLayout createRecording = findViewById(R.id.create_recording);
         createRecording.setVisibility(View.VISIBLE);
 
-        mediaRecorder.stop();
-        mediaRecorder.release();
-        mediaRecorder = null;
+        mediaPlayer.stop();
+        mediaPlayer.release();
+        mediaPlayer = null;
     }
 
     public void playAudio(View v, File file) {
+        if (!checkPermission()) {
+            requestPermission();
+            return;
+        }
+
         currentlyPlaying.setVisibility(View.VISIBLE);
         currentlyPlayingAudio.setVisibility(View.VISIBLE);
         currentlyPlayingAudio.setText(file.getName());
@@ -181,8 +184,7 @@ public class RecordActivity extends AppCompatActivity {
     private void updateAudioFiles() {
         File[] files = getFiles();
         if (files == null) {
-            TextView noResults = findViewById(R.id.no_recordings);
-            noResults.setVisibility(View.VISIBLE);
+            noRecordings.setVisibility(View.VISIBLE);
         } else {
             ListView listView = findViewById(R.id.list_recordings);
             RecordingAdapter adapter = new RecordingAdapter(this, files);
@@ -190,34 +192,46 @@ public class RecordActivity extends AppCompatActivity {
         }
     }
 
-    /**
-     * Check user has given microphone permissions
-     * @return True if microphone permissions are given
-     */
-    protected boolean hasMicrophone() {
-        PackageManager packageManager = this.getPackageManager();
-        return packageManager.hasSystemFeature(PackageManager.FEATURE_MICROPHONE);
+
+    private void requestPermission() {
+        ActivityCompat.requestPermissions(RecordActivity.this, new
+                String[]{WRITE_EXTERNAL_STORAGE, RECORD_AUDIO, READ_EXTERNAL_STORAGE}, RequestPermissionCode);
     }
 
-    /**
-     * Checks if the app has permission to write to device storage
-     *
-     * If the app does not has permission then the user will be prompted to grant permissions
-     *
-     * @param activity
-     */
-    public void verifyStoragePermissions(Activity activity) {
-        // Check if we have write permission
-        int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case RequestPermissionCode:
+                if (grantResults.length> 0) {
+                    boolean StoragePermission = grantResults[0] ==
+                            PackageManager.PERMISSION_GRANTED;
+                    boolean RecordPermission = grantResults[1] ==
+                            PackageManager.PERMISSION_GRANTED;
+                    boolean ReadPermission = grantResults[2] ==
+                            PackageManager.PERMISSION_GRANTED;
 
-        if (permission != PackageManager.PERMISSION_GRANTED) {
-            // We don't have permission so prompt the user
-            ActivityCompat.requestPermissions(
-                    activity,
-                    PERMISSIONS_STORAGE,
-                    REQUEST_EXTERNAL_STORAGE
-            );
+                    if (StoragePermission && RecordPermission && ReadPermission) {
+                        Toast.makeText(RecordActivity.this, "Permission Granted",
+                                Toast.LENGTH_LONG).show();
+                    } else {
+                        Toast.makeText(RecordActivity.this,"Permission Denied",Toast.LENGTH_LONG).show();
+                    }
+                }
+                break;
         }
+    }
+
+    public boolean checkPermission() {
+        int result = ContextCompat.checkSelfPermission(getApplicationContext(),
+                WRITE_EXTERNAL_STORAGE);
+        int result1 = ContextCompat.checkSelfPermission(getApplicationContext(),
+                RECORD_AUDIO);
+        int result2 = ContextCompat.checkSelfPermission(getApplicationContext(),
+                READ_EXTERNAL_STORAGE);
+        return result == PackageManager.PERMISSION_GRANTED &&
+                result1 == PackageManager.PERMISSION_GRANTED &&
+                result2 == PackageManager.PERMISSION_GRANTED;
     }
 
     /**
